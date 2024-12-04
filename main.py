786a1768dc1cdba34e56198f28207fa89d596340
Alexandr <sevrjugin@bk.ru>
2024-12-05 02:09:20 +0300

    refacted sql tables

--- main.py
+++ main.py
@@ -20,22 +20,61 @@ def get_db():
     finally:
         db.close()
 
-
-
-@app.get("/api/submitData")
-def submitData():
-    return "{ ""status"": 500, ""message"": ""Use POST methood"",""id"": 'null'}"
-
-
+def get_cur_id(db,model):
+    if db.query(model).order_by(text("id desc")).first():
+        return db.query(model).order_by(text("id desc")).first().id + 1
+    else:
+        return 1
 
 @app.post("/api/submitData")
 def submitData(data=Body(), db: Session = Depends(get_db)):
      current_date = datetime.now()
-     id = db.query(Pereval_added).order_by(text("id desc")).first().id+1
      raw_data = json.dumps({k: v for k, v in data.items() if k != "images"})
-     pereval = Pereval_added(id = id, date_added=current_date, raw_data=raw_data, images="")
+
+     usid = get_cur_id(db,Users)
+
+     user = Users(id = usid,
+                  email = data["user"]["email"],
+                  phone = data["user"]["phone"],
+                  fam   = data["user"]["fam"],
+                  name  = data["user"]["name"],
+                  otc   = data["user"]["otc"])
+     db.add(user)
+
+     chid = get_cur_id(db,Choords)
+     choords = Choords(id = chid,
+                       latitude  = data["coords"]["latitude"],
+                       longitude = data["coords"]["longitude"],
+                       height    = data["coords"]["height"])
+     db.add(choords)
+
+     id = get_cur_id(db,Pereval_added)
+     pereval = Pereval_added(id = id,
+                             beautyTitle=data["beauty_title"],
+                             title=data["title"],
+                             other_titles=data["other_titles"],
+                             connect=data["connect"],
+                             user=usid,
+                             choords=chid,
+                             level_winter=data["level"]["winter"],
+                             level_summer=data["level"]["summer"],
+                             level_autumn=data["level"]["autumn"],
+                             level_spring=data["level"]["spring"],
+                             date_added=current_date)
      db.add(pereval)
+     for img in data["images"]:
+         imid = get_cur_id(db,Pereval_images)
+         pereval_images = Pereval_images(id=imid,
+                                         title = img["title"],
+                                         date_added=current_date,
+                                         img=img["data"])
+         pimid = get_cur_id(db,Pereval_pereval_images)
+         Ppimages = Pereval_pereval_images(id=pimid,
+                                           id_pereval=id,
+                                           id_image=imid)
+         db.add(Ppimages)
+
      db.commit()
-     db.refresh(pereval)
      return json.dumps({ "status": 200, "message": 'null', "id": pereval.id })
+
 
